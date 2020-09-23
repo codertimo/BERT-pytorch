b5b44e57fc5a7c4a4d2ce5d412c1eecf1e8276ff
dshwei <42167236+dshwei@users.noreply.github.com>
2020-09-23 14:47:32 +0800

    Update transformer.py

--- bert_pytorch/model/transformer.py
+++ bert_pytorch/model/transformer.py
@@ -27,5 +27,6 @@ class TransformerBlock(nn.Module):
 
     def forward(self, x, mask):
         x = self.input_sublayer(x, lambda _x: self.attention.forward(_x, _x, _x, mask=mask))
-        x = self.output_sublayer(x, self.feed_forward)
+#         x = self.output_sublayer(x, self.feed_forward)
+        x = self.output_sublayer(x, lambda _x: self.feed_forward.forward(_x))
         return self.dropout(x)
